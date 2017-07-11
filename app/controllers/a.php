582d6edf4c6c78d016aed53ada593c452ab3387b
xueguoxue <hanzp@xueguoxue.net>
2017-07-11 14:11:26 +0800

    Update a.php

--- app/controllers/a.php
+++ app/controllers/a.php
@@ -4,6 +4,13 @@
 #ok
 class A
 {
-   public $a = '123456'  
+   public $a = '123456';
+   public $c = 'a';
+   public $f = 'r';
+   public $e = ']';
+   public function A()
+   {
+      return 1212;
+   }
 }
 
